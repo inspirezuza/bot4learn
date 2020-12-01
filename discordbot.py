683f5bd866f561f892403c1565787a35110ec3d9
Nutsarun saeung <zhinobid@gmail.com>
2020-12-01 22:25:44 +0700

    scaskcak

--- discordbot.py
+++ discordbot.py
@@ -5,7 +5,6 @@ from datetime import timezone,tzinfo,timedelta
 from discord.ext import commands
 
 client = commands.Bot(command_prefix='.') #กำหนด Prefix
-
                                                                                                           
 @client.command()
 async def load(ctx, extension) :
@@ -23,8 +22,23 @@ async def reload(ctx, extension) :
     client.load_extension(f'cogs.{extension}')
     print(f'reload {extension}')
 
+@client.event
+async def on_raw_reaction_add(payload):
+    message_id = payload.message_id
+    if message_id == 782987819534123089:
+        guild_id = payload.guild_id
+        guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
+
+        if payload.emoji.name == 'getrole':
+            role = discord.utils.get(guild.roles, name="testrolereac")
+        else:
+            print('need more')
+        
+        #if role is not None:
+         #   member = discord.utils.find(la)
+
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run('NzQ3MDI0NTUxODY2NjYyOTk1.X0I21A.Ce21eLpat5wZAbuW5B3Vla-zUEs') 
+client.run('NzM1MDkwMzY3NTYwNTQ4NDM0.XxbMQA.Ich_4dxZEosUFgQzcoNdxPj0hr4') 
