b864903b3f342d60a44b79217f8dd21383948fa2
Nutsarun saeung <zhinobid@gmail.com>
2020-12-29 14:14:17 +0700

    @rolereac press

--- cogs/rolereac.py
+++ cogs/rolereac.py
@@ -1,33 +1,39 @@
 import discord
+import random
 from discord.ext import commands
 
 class rolereac(commands.Cog):
 
     def __init__(self, client):
         self.client = client
+        self.reaction_author = None
+        self.message_author = None
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
         message_id = payload.message_id
         channel_id = payload.channel_id
         channel = self.client.get_channel(channel_id)
+        self.reaction_author = payload.user_id
         if message_id == 790159697213784094:
-            # channel = self.client.get_channel('role-react')
-            # guild_id = payload.guild_id
-            # guild = discord.utils.find(lambda g : g.id == guild_id, self.client.guilds)
-
             if payload.emoji.name == 'getrole':
-                print('nice one')
-                await channel.send('EZ')
-
-                # await ctx.send('เห้อ')
-
+                print(self.reaction_author)
+                await channel.send('Press number for random')
             elif payload.emoji.name == 'getrole2':
                 print('nice two')
 
     @commands.Cog.listener()
+    async def on_message(self, message):
+        self.message_content = message.content
+        self.message_userid = message.author.id
+        self.message_author = message.author
+        if self.reaction_author == self.message_userid:
+            await message.channel.send(random.randint(1, int(message.content)))
+            self.reaction_author = None
+            
+    @commands.Cog.listener()
     async def on_raw_reaction_remove(self, payload):
-            pass               
+        pass               
 
     @commands.command()
     async def set_reaction(self, ctx, msg: discord.Message=None, emoji=None):
